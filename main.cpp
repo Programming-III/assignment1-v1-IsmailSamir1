b96c0f445b1404322eab08c18a8303ae3c160a61
IsmailSamir1 <ismailahmed1132006@gmail.com>
2025-11-12 08:35:03 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,7 +10,19 @@ using namespace std;
 
 // ============== MAIN FUNCTION ==============
 int main() {
-    
+
+    Animal a=Animal("Lion",5,true);
+    Animal p=Animal("Parrot",2,false);
+    Animal S=Animal("Snake",3,true);
+    Animal A*[]={a,p,S};
+   Enclosure E=Enclosure(5,3,A);
+   E.addAnimal("Elephant");
+   E.addAnimal("Eagle");
+   E.addAnimal("Crocodile");
+   Visitor v=Visitor("Sara Ali",3);
+   v.displayinfo();
+   E.displayAnimals();
+   
     
     return 0;
 }
